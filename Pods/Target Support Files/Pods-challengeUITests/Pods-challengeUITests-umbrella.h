0242426e4000da3a247699ac4dcbb146517d22d2
andrewcameron <andrewc@mediswitch.co.za>
2017-03-10 07:49:46 +0200

    code

---
+++ Pods/Target Support Files/Pods-challengeUITests/Pods-challengeUITests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_challengeUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_challengeUITestsVersionString[];
+
